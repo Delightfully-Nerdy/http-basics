a1175607fe85b632c82f3622d3d1d0b8df1be0b6
Dalitso Kassonde <dkasonde11@gmail.com>
2023-05-24 10:35:32 +0200

    header actions

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const path = require('path');
+const send = require('send');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, resn) => {
-    res.send('<h1>Hello from express1</h1>');
+app.delete('/post/:id', (req, res) => {
+    //Database stuff
+
+    res.json({ msg: `Post ${req.params.id} deleted`});
+
 });
+
 
 app.listen(5000, () => console.log('Server started on 5000'));
